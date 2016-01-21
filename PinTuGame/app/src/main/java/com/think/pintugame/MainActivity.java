d315687ed341c6fc568942680ea1fdbffea97fd2
Zous <nimdanoob@163.com>
2016-01-21 13:30:45 +0800

    拼图游戏 提交

--- PinTuGame/app/src/main/java/com/think/pintugame/MainActivity.java
+++ PinTuGame/app/src/main/java/com/think/pintugame/MainActivity.java
@@ -1,13 +1,71 @@
 package com.think.pintugame;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.think.pintugame.view.GamePintuLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private GamePintuLayout mGamepintuLayout;
+    private TextView mLevel;
+    private TextView mTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGamepintuLayout = (GamePintuLayout) findViewById(R.id.id_gamePintu);
+        mTime = (TextView) findViewById(R.id.id_time);
+        mLevel = (TextView) findViewById(R.id.id_level);
+        mGamepintuLayout.setIsTimeEnable(true);
+        mGamepintuLayout.setOnGamePintuListener(new GamePintuLayout.GamePintuListener() {
+            @Override
+            public void nextLevel(final int nextLevel) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Game info")
+                        .setMessage("Level UP!!!").setPositiveButton("NEXT LEVEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mGamepintuLayout.nextLevel();
+                        mLevel.setText(""+nextLevel);
+                    }
+                }).show();
+            }
+
+            @Override
+            public void timeChanged(int currentTime) {
+                mTime.setText(""+currentTime);
+            }
+
+            @Override
+            public void gameOver() {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Game Over")
+                        .setMessage("The Time is  UP!!!").setPositiveButton("RESTART", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mGamepintuLayout.reStart();
+                    }
+                }).setNegativeButton("QUIT", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGamepintuLayout.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGamepintuLayout.pause();
     }
 }
--- PinTuGame/app/src/main/java/com/think/pintugame/view/GamePintuLayout.java
+++ PinTuGame/app/src/main/java/com/think/pintugame/view/GamePintuLayout.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.think.pintugame.R;
 import com.think.pintugame.utils.ImagePiece;
@@ -44,6 +50,8 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
     private int mWidth;
 
 
+    private boolean isGameSuccess;
+    private int mTime = 20;
     /**
      * 游戏的原始图片
      */
@@ -51,8 +59,74 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     private List<ImagePiece> mItemBitmaps;
 
+    private RelativeLayout mAnimLayout;
+
     private boolean once;
     private int mItemWidth;
+    private boolean isAnim;
+
+    public interface GamePintuListener{
+        void nextLevel(int nextLevel);
+        void timeChanged(int currentTime);
+        void gameOver();
+    }
+
+    public GamePintuListener mListener;
+
+    /**
+     * 设置接口回调
+     * @param mListener
+     */
+    public void setOnGamePintuListener(GamePintuListener mListener) {
+        this.mListener = mListener;
+    }
+    private int level =1;
+    private static final int TIME_CHANGED = 0X110;
+    private static final int NEXT_LEVLE = 0x111;
+    /**
+     * 是否设置开启时间
+     */
+    private boolean isTimeEnable;
+    private boolean isGameOver;
+
+    public void setIsTimeEnable(boolean isTimeEnable) {
+        this.isTimeEnable = isTimeEnable;
+    }
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case TIME_CHANGED:
+                    if (isGameSuccess || isGameOver || isPause){
+                        return;
+                    }
+
+                    if (mListener != null){
+                        mListener.timeChanged(mTime);
+                        if (mTime == 0){
+                            isGameOver = true;
+                            mListener.gameOver();
+                            return;
+                        }
+                    }
+                    mTime--;
+                    mHandler.sendEmptyMessageDelayed(TIME_CHANGED,1000);
+                    break;
+                case NEXT_LEVLE:
+                    level++;
+                    if (mListener!=null) {
+                        mListener.nextLevel(level);
+                    } else {
+                        nextLevel();
+                    }
+                    break;
+
+            }
+            super.handleMessage(msg);
+        }
+    };
+
 
     public GamePintuLayout(Context context) {
         this(context,null);
@@ -78,8 +152,24 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
             initBitmap();
             //设置ImageView(Item)的宽高属性
             initItem();
+
+            //判断是否开启时间
+            checkTimeEnable();
         }
         setMeasuredDimension(mWidth, mWidth);
+    }
+
+    private void checkTimeEnable() {
+        if (isTimeEnable){
+            //根据当前等级设置时间
+            countTimeBaseLevel();
+            mHandler.sendEmptyMessage(TIME_CHANGED);
+        }
+    }
+
+    private void countTimeBaseLevel() {
+        mTime = (int) Math.pow(2,level)*60;//2的多少倍 -》也就是事件长度指数增加
+        mHandler.sendEmptyMessage(TIME_CHANGED);
     }
 
 
@@ -88,7 +178,7 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
             mBitamp = BitmapFactory.decodeResource(getResources(), R.mipmap.image);
         }
 
-        mItemBitmaps = ImageSplitterUtil.splitImage(mBitamp,3);
+        mItemBitmaps = ImageSplitterUtil.splitImage(mBitamp,mColumn);
         //todo  如何随机排序
         Collections.sort(mItemBitmaps, new Comparator<ImagePiece>() {
             @Override
@@ -169,6 +259,10 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
+        if (isAnim)
+            return;
+
         //当时2次点击一个 第一个 Item时
         if (mFirst == v){
             mFirst.setColorFilter(null);
@@ -187,29 +281,147 @@ public class GamePintuLayout extends RelativeLayout implements View.OnClickListe
 
     private void exchangeView() {
         mFirst.setColorFilter(null);
-        String firstTag = (String) mFirst.getTag();
-        String secondTag = (String)mSecond.getTag();
-        String[] firstParams = firstTag.split("_");
-        String[] secondParams = secondTag.split("_");
-        Bitmap firstBitmap = mItemBitmaps.get(Integer.parseInt(firstParams[0])).getBitmap();
-        mSecond.setImageBitmap(firstBitmap);
 
-        Bitmap secondBitmap = mItemBitmaps.get(Integer.parseInt(secondParams[0])).getBitmap();
-        mFirst.setImageBitmap(secondBitmap);
+        setUpAnimLayout();
 
-//        Drawable firstDrawable = mFirst.getDrawable();
-//        Drawable secondDrawable = mSecond.getDrawable();
-//        mFirst.setImageDrawable(secondDrawable);
-//        mSecond.setImageDrawable(firstDrawable);
+        final ImageView first = new ImageView(getContext());
+        final Bitmap firstBitmap = mItemBitmaps.get(getImageIdByTag((String) mFirst.getTag())).getBitmap();
 
+        first.setImageBitmap(firstBitmap);
+        RelativeLayout.LayoutParams lp = new LayoutParams(mItemWidth,mItemWidth);
+        lp.leftMargin = mFirst.getLeft() - mPadding;//动画层是在padding里面的，所以要减去padding
+        lp.topMargin = mFirst.getTop() - mPadding;
+        first.setLayoutParams(lp);
+        mAnimLayout.addView(first);
 
-//        //改变tag
-//        String tempTag = (String) mFirst.getTag();
-//        mFirst.setTag(mSecond.getTag());
-//        Log.w("logger", mFirst.getTag() + "");
-//        mSecond.setTag(tempTag);
-//        Log.w("logger", mSecond.getTag() + "");
+        ImageView second = new ImageView(getContext());
+        final Bitmap secondBitmap = mItemBitmaps.get(getImageIdByTag((String) mSecond.getTag())).getBitmap();
 
-        mFirst = mSecond = null;
+        second.setImageBitmap(secondBitmap);
+        RelativeLayout.LayoutParams lp2 = new LayoutParams(mItemWidth,mItemWidth);
+        lp2.leftMargin = mSecond.getLeft() - mPadding;//动画层是在padding里面的，所以要减去padding
+        lp2.topMargin = mSecond.getTop() - mPadding;
+        second.setLayoutParams(lp2);
+        mAnimLayout.addView(second);
+
+        //设置动画
+        TranslateAnimation anim = new TranslateAnimation(0,mSecond.getLeft()-mFirst.getLeft(),0,mSecond.getTop()-mFirst.getTop());
+        anim.setDuration(300);
+        anim.setFillAfter(true);
+        first.startAnimation(anim);
+
+        TranslateAnimation animSecond = new TranslateAnimation(0,-mSecond.getLeft()+mFirst.getLeft(),0,-mSecond.getTop()+mFirst.getTop());
+        animSecond.setDuration(300);
+        animSecond.setFillAfter(true);
+        second.startAnimation(animSecond);
+
+        //监听动画
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mFirst.setVisibility(View.INVISIBLE);
+                mSecond.setVisibility(View.INVISIBLE);
+                isAnim = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                String firstTag = (String) mFirst.getTag();
+                String secondTag = (String) mSecond.getTag();
+                mFirst.setImageBitmap(secondBitmap);
+                mSecond.setImageBitmap(firstBitmap);
+
+                mFirst.setTag(secondTag);
+                mSecond.setTag(firstTag);
+
+                mFirst.setVisibility(VISIBLE);
+                mSecond.setVisibility(VISIBLE);
+
+                mAnimLayout.removeAllViews();
+                mFirst = mSecond = null;
+                //判断用户游戏是否成功
+                checkSuccess();
+                isAnim = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    /**
+     * 判断用户游戏是否成功
+     */
+    private void checkSuccess() {
+        boolean isSuccess  = true;
+        for (int i = 0;i < mGamePintuItems.length;i++){
+            ImageView imageView  = mGamePintuItems[i];
+            if (getImageIndexByTag((String) imageView.getTag()) != i){
+                isSuccess  = false;
+            }
+        }
+        if (isSuccess){
+            Log.w("logger","is Success");
+            isGameSuccess = true;
+            mHandler.removeMessages(TIME_CHANGED);
+
+            Toast.makeText(getContext(),"Success ,Game Level Up",Toast.LENGTH_LONG).show();
+            mHandler.sendEmptyMessage(NEXT_LEVLE);
+        }
+    }
+
+    public void reStart(){
+        isGameOver = false;
+        mColumn --;
+        nextLevel();
+    }
+
+    private boolean isPause;
+    public void pause(){
+        isPause = true;
+        mHandler.removeMessages(TIME_CHANGED);
+    }
+
+    public void resume(){
+        if (isPause){
+            isPause = false;
+            mHandler.sendEmptyMessage(TIME_CHANGED);
+        }
+    }
+    public void nextLevel(){
+        this.removeAllViews();
+        mAnimLayout = null;
+        mColumn++;
+        isGameSuccess = false;
+        checkTimeEnable();
+        initBitmap();
+        initItem();
+    }
+    /**
+     * 根据 tag  获取Id
+     * @param tag
+     * @return 图片id
+     */
+    public int getImageIdByTag(String tag){
+        String[] split = tag.split("_");
+        return Integer.parseInt(split[0]);
+    }
+
+    public int getImageIndexByTag(String tag){
+        String[] split = tag.split("_");
+        return Integer.parseInt(split[1]);
+    }
+
+    /**
+     * 构造动画层
+     */
+    private void setUpAnimLayout() {
+        if (mAnimLayout == null){
+            mAnimLayout = new RelativeLayout(getContext());
+            addView(mAnimLayout);
+        }
     }
 }
